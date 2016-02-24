cf09a6d247e316819cd23ccc042710e81081c7a4
Thomas Petrachi <tpetrachizago@gmail.com>
2016-02-24 13:45:01 +0100

    Add Token & Lexer

---
+++ saphyr/token.rb
@@ -0,0 +1,16 @@
+module Saphyr
+  class Token
+    attr_accessor :type, :content, :line, :column
+
+    def initialize type, content, line, column
+      @type = type
+      @content = content
+      @line = line
+      @column = column
+    end
+
+    def inspect
+      "#{@type}: #{@content.inspect} at [#{@line}, #{@column}]"
+    end
+  end
+end
