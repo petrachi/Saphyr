f2951399d690b7a00222e05a66f56382e723f85c
Thomas Petrachi <tpetrachizago@gmail.com>
2016-02-24 14:56:48 +0100

    Improve Node & Parser

--- saphyr/node.rb
+++ saphyr/node.rb
@@ -1,10 +1,12 @@
 module Saphyr
   class Node
-    attr_accessor :token, :level, :children
+    attr_accessor :token, :level, :parent, :children
 
     def initialize token
       @token = token
       @level = 0
+
+      @parent = nil
       @children = []
     end
 
@@ -14,12 +16,41 @@ module Saphyr
 
     def add_node node
       node.level = level + 1
+      node.parent = self
       children << node
+
       node
     end
 
+    def swap token
+      swap_node Node.new(token)
+    end
+
+    def swap_node node
+      # p self
+      # p self.parent
+
+      parent.children.delete(self)
+
+      node.level = level
+      node.parent = parent
+      node.children = [self] + children
+
+      parent.children << node
+
+      self.parent = node
+      self.children = []
+      self.level += 1
+
+      node
+    end
+
+    def last_child
+      @children.last
+    end
+
     def inspect
-      "#{ "\t"*@level }#{ @token.content }\n" + children.map(&:inspect).join("\n")
+      "#{ "\t"*@level }#{ @token.content };\n" + children.map(&:inspect).join
     end
   end
 end
--- saphyr/parser.rb
+++ saphyr/parser.rb
@@ -2,63 +2,62 @@ module Saphyr
   class Parser
     def initialize source_code
       @lexer = Lexer.new source_code
-      @root = Node.new nil
+      @root = Node.new Token.new('', '', 0, 0)
     end
 
     def parse
       @lexer.tokenize
+
       begin
-        token = @lexer.get
-
-        case token.type
-        when 'IDENTIFIER'
-          parse_identifier @root, token
-        when 'NUMBER'
-          parse_number @root, token
-        when 'STRING'
-          parse_string @root, token
-        else
-          raise "Improper syntax near #{token.inspect}"
-        end
-
+        parse_token @root
       end while @lexer.peek
     end
 
-    def parse_identifier parent_node, token
-      case @lexer.peek.type
+    def parse_token parent_node
+      token = @lexer.get
+
+      # p "parsing #{token.content}"
+
+      case token.type
       when 'SYMBOL'
+        # p "parse symbol"
         parse_symbol parent_node, token
-      else
-        raise "Improper syntax near #{@lexer.peek.inspect}"
+      when 'IDENTIFIER'
+        # p "parse identifier"
+        parse_identifier parent_node, token
+      when 'NUMBER'
+        # p "parse number"
+        parse_number parent_node, token
+      when 'STRING'
+        # p "parse string"
+        parse_string parent_node, token
       end
+
+      # p "parsed, AST now"
+      # p @root
     end
 
     def parse_symbol parent_node, token
-      case @lexer.peek.content
+      case token.content
       when "\n"
         # stop
       else
-        node = parent_node.add @lexer.get
-        node.add token
+        # p "will swap"
+        # p parent_node
+        # p parent_node.last_child
+        # p token
 
+        node = parent_node.last_child.swap token
+        # p "parsed, AST now"
+        # p @root
         begin
-          token = @lexer.get
-
-          case token.type
-          when 'IDENTIFIER'
-            parse_identifier node, token
-          when 'NUMBER'
-            parse_number node, token
-          when 'STRING'
-            parse_string node, token
-          when 'SYMBOL'
-            parse_symbol node, token
-          else
-            raise "Improper syntax near #{token.inspect}"
-          end
-
-        end while token.content != "\n"
+          parse_token node
+        end while @lexer.current.content != "\n"
       end
+    end
+
+    def parse_identifier parent_node, token
+      parent_node.add token
     end
 
     def parse_number parent_node, token
@@ -68,5 +67,69 @@ module Saphyr
     def parse_string parent_node, token
       parent_node.add token
     end
+
+    # def parse
+    #   @lexer.tokenize
+    #   begin
+    #     token = @lexer.get
+    #
+    #     case token.type
+    #     when 'IDENTIFIER'
+    #       parse_identifier @root, token
+    #     when 'NUMBER'
+    #       parse_number @root, token
+    #     when 'STRING'
+    #       parse_string @root, token
+    #     else
+    #       raise "Improper syntax near #{token.inspect}"
+    #     end
+    #
+    #   end while @lexer.peek
+    # end
+    #
+    # def parse_identifier parent_node, token
+    #   case @lexer.peek.type
+    #   when 'SYMBOL'
+    #     parse_symbol parent_node, token
+    #   else
+    #     raise "Improper syntax near #{@lexer.peek.inspect}"
+    #   end
+    # end
+    #
+    # def parse_symbol parent_node, token
+    #   case @lexer.peek.content
+    #   when "\n"
+    #     # stop
+    #   else
+    #     node = parent_node.add @lexer.get
+    #     node.add token
+    #
+    #     begin
+    #       token = @lexer.get
+    #
+    #       case token.type
+    #       when 'IDENTIFIER'
+    #         parse_identifier node, token
+    #       when 'NUMBER'
+    #         parse_number node, token
+    #       when 'STRING'
+    #         parse_string node, token
+    #       when 'SYMBOL'
+    #         parse_symbol node, token
+    #       else
+    #         raise "Improper syntax near #{token.inspect}"
+    #       end
+    #
+    #     end while token.content != "\n"
+    #   end
+    # end
+    #
+    # def parse_number parent_node, token
+    #   parent_node.add token
+    # end
+    #
+    # def parse_string parent_node, token
+    #   parent_node.add token
+    # end
   end
 end
